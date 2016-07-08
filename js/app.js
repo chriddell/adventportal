8885692416c44adfabde5022416109c85adc34a3
Chris Liddell <nowthen@chrisliddell.co.uk>
2016-07-08 12:14:31 +0100

    Advent global header in.

--- js/app.js
+++ js/app.js
@@ -458,4 +458,30 @@ function closeOverlay(clicked) {
 
   });
 
+  $(window).on( "orientationchange", function() {
+    showOverlay('#show-refresh-message');
+  });
+
+  // Add body class if iOS
+  if( navigator.userAgent.match(/iP(hone|od|ad)/i) ) {
+    $('body').addClass('ios');
+  }
+
+  //open/close primary navigation
+  $('.cd-primary-nav-trigger, .close-trigger').on('click', function(){
+    $('.cd-menu-icon').toggleClass('is-clicked'); 
+    $('header').toggleClass('menu-is-open');
+    
+    //in firefox transitions break when parent overflow is changed, so we need to wait for the end of the trasition to give the body an overflow hidden
+    if( $('.cd-primary-nav').hasClass('is-visible') ) {
+      $('.cd-primary-nav').removeClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').removeClass('overflow-hidden');
+      });
+    } else {
+      $('.cd-primary-nav').addClass('is-visible').one('webkitTransitionEnd otransitionend oTransitionEnd msTransitionEnd transitionend',function(){
+        $('body').addClass('overflow-hidden');
+      }); 
+    }
+  });
+
 })()
